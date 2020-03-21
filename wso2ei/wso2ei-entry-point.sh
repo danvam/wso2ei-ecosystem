974a31e8e42fadfed78b49ea74ed394c04bb155c
Daniel Valle <danvam@github.com>
2020-03-21 19:21:56 +0100

    fist commit

---
+++ wso2ei/wso2ei-entry-point.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#wso2ei-entrypoint.sh
+cp /home/wso2carbon/custom/lib/* /home/wso2carbon/wso2ei-6.5.0/lib/
+#cp /home/wso2carbon/custom/dropins/* /home/wso2carbon/wso2ei-6.5.0/dropins/
+cp -R /home/wso2carbon/custom/conf/* /home/wso2carbon/wso2ei-6.5.0/conf
+# 6.6.0
+#/home/wso2carbon/docker-entrypoint.sh
+# 6.5.0
+/home/wso2carbon/init.sh
