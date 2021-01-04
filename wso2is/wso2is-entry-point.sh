1328b11b70d0a8d63d356819dac405452b99eac2
María José Sánchez <mariajose.sanchez@aeptic.es>
2021-01-04 19:53:41 +0100

    integración identity server

--- wso2is/wso2is-entry-point.sh
+++ wso2is/wso2is-entry-point.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #wso2is-entrypoint.sh
-#cp /home/wso2carbon/common/lib/* /home/wso2carbon/wso2is-5.8.0/lib/
-#cp /home/wso2carbon/custom/dropins/* /home/wso2carbon/wso2ei-6.5.0/dropins/
 cp -R /home/wso2carbon/common/conf/* /home/wso2carbon/wso2is-5.8.0/conf
+cp /home/wso2carbon/custom/repository/resources/security/* /home/wso2carbon/wso2is-5.8.0/repository/resources/security/
+cp /home/wso2carbon/custom/repository/resources/conf/* /home/wso2carbon/wso2is-5.8.0/repository/resources/conf/
 # 5.8.0
 /home/wso2carbon/init.sh
