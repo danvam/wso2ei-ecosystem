9542519b08e80b2aef38177ae62ac2084300bff0
María José Sánchez <mariajose.sanchez@aeptic.es>
2021-01-24 19:32:52 +0100

    authenticacion ntlm

--- wso2ei/wso2ei-entry-point.sh
+++ wso2ei/wso2ei-entry-point.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #wso2ei-entrypoint.sh
 cp /home/wso2carbon/custom/lib/* /home/wso2carbon/wso2ei-6.5.0/lib/
+cp /home/wso2carbon/custom/dropins/* /home/wso2carbon/wso2ei-6.5.0/dropins/
 cp /home/wso2carbon/custom/repository/resources/security/* /home/wso2carbon/wso2ei-6.5.0/repository/resources/security/
 cp -R /home/wso2carbon/custom/conf/* /home/wso2carbon/wso2ei-6.5.0/conf
 # 6.6.0
